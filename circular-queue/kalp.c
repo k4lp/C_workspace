1c6bdf44d3d05fa585605cefc2861e844066bbe7
k4lp <kalppariya1234@gmail.com>
2020-12-14 17:25:09 +0530

    done

--- circular-queue/kalp.c
+++ circular-queue/kalp.c
@@ -6,3 +6,19 @@ typedef struct _node {
 } node;
 
 node *pu;
+
+void initialize() {
+	pu = (node *)malloc(sizeof(node));
+	printf("\nnEnter the size of the queue :- ");
+	scanf(" %d",&pu->size);
+	pu->q = (int *)malloc(sizeof(int) * pu->size +1);
+	pu->front = pu->rear = 0;
+}
+
+int isempty() {
+	return (pu->front == pu->rear);
+}
+
+int isfull() {
+	return ((pu->rear + 1) % pu->size == pu->front);
+}
